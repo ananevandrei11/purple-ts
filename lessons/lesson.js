407ba08a962c3135a3d575f54bb19496c09ed028
Ananev Andrei <35199801+ananevandrei11@users.noreply.github.com>
2023-07-16 16:22:21 +0200

    L-3

--- lessons/lesson.js
+++ lessons/lesson.js
@@ -25,7 +25,7 @@ var StatusCode;
     StatusCode["published"] = "published";
     StatusCode["deleted"] = "deleted";
 })(StatusCode || (StatusCode = {}));
-const user = "user" /* USER */;
+const user = "user" /* Roles.USER */;
 function getFaqs(req) {
     return __awaiter(this, void 0, void 0, function* () {
         const res = yield fetch('/faqs', {
@@ -40,4 +40,67 @@ const a = getFaqs({
     topicId: 1,
     status: StatusCode.published,
 });
-a.then((res) => console.log(res.status));
+a.then((res) => console.log(res[0].status));
+// UNION
+function logError(err) {
+    let errors = '';
+    if (Array.isArray(err)) {
+        err.forEach((e) => (errors += e));
+        return errors;
+    }
+    else {
+        return err;
+    }
+}
+function logObject(obj) {
+    if ('a' in obj) {
+        console.log(obj.a);
+    }
+    else {
+        console.log(obj.b);
+    }
+}
+function logMultipleIds(a, b) {
+    if (typeof a === typeof b) {
+        console.log('as string');
+    }
+    else {
+        console.log('as other');
+    }
+}
+function fetchWithAuth(url, method) {
+    return __awaiter(this, void 0, void 0, function* () {
+        try {
+            const res = yield fetch(url, {
+                method,
+            });
+            return res.json();
+        }
+        catch (e) {
+            return String(e);
+        }
+    });
+}
+/*
+let methodError = 'POST';
+fetchWithAuth('', methodError); // with error types
+*/
+const methodSuccess = 'POST';
+fetchWithAuth('', methodSuccess); // without error types
+let someUser = {
+    name: 'name',
+    age: 33,
+    skills: ['DevOps', 'Dev'],
+};
+let someUserWithId = {
+    name: 'name',
+    age: 33,
+    skills: ['DevOps', 'Dev'],
+    id: 1,
+};
+function multiply(f, s) {
+    if (!s) {
+        return f * f;
+    }
+    return f * s;
+}
