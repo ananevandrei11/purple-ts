f1d95164c69c6f5a16bf2a4d723f6c461d354dac
ananevandrei11 <ujlpjcbjlf2011@gmail.com>
2023-08-21 20:56:37 +0200

    12 patterns clone

--- src/lessons/patterns/index.ts
+++ src/lessons/patterns/index.ts
@@ -37,3 +37,28 @@
   new Service1().addMap(1, 'Works');
   new Service2().getKeys(1);
 })();
+
+// Prototype
+(function () {
+  interface Prototype<T> {
+    clone(): T;
+  }
+
+  class UserHistory implements Prototype<UserHistory> {
+    createdAt: Date;
+    constructor(public email: string, public name: string) {
+      this.createdAt = new Date();
+    }
+    clone(): UserHistory {
+      let target = new UserHistory(this.email, this.name);
+      target.createdAt = this.createdAt;
+      return target;
+    }
+  }
+
+  let user1 = new UserHistory('a@a.com', 'Andrei');
+  let user2 = user1.clone();
+  user2.email = 'b@b.com';
+  console.log(user2);
+  console.log(user1);
+})();
