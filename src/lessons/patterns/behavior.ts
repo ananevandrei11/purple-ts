674974cddb704dab98c7e666a84389fb28810834
ananevandrei11 <ujlpjcbjlf2011@gmail.com>
2023-08-29 22:59:41 +0200

    behavior patterns 3

--- src/lessons/patterns/behavior.ts
+++ src/lessons/patterns/behavior.ts
@@ -327,3 +327,88 @@
   console.log(auth.authUser(user));
   */
 })();
+
+// ITERATOR
+(function () {
+  class Task {
+    constructor(public priorety: number) {}
+  }
+
+  class TaskList {
+    private tasks: Task[] = [];
+
+    public sortByPriorety() {
+      this.tasks = this.tasks.sort((a, b) => {
+        if (a.priorety > b.priorety) {
+          return 1;
+        }
+        if (a.priorety < b.priorety) {
+          return -1;
+        }
+        return 0;
+      });
+    }
+    addTask(task: Task) {
+      this.tasks.push(task);
+    }
+
+    public getTasks() {
+      return this.tasks;
+    }
+
+    public count() {
+      return this.tasks.length;
+    }
+
+    public getIterator(): IIterator<Task> {
+      return new PrioretyIterator(this);
+    }
+  }
+
+  interface IIterator<T> {
+    current(): T | undefined;
+    next(): T | undefined;
+    prev(): T | undefined;
+    index(): number;
+  }
+
+  class PrioretyIterator implements IIterator<Task> {
+    private position: number = 0;
+    private taskList: TaskList;
+
+    constructor(taskList: TaskList) {
+      // priorety
+      taskList.sortByPriorety();
+      this.taskList = taskList;
+    }
+    current(): Task | undefined {
+      return this.taskList.getTasks()[this.position];
+    }
+    next(): Task | undefined {
+      this.position += 1;
+      return this.taskList.getTasks()[this.position];
+    }
+    prev(): Task | undefined {
+      this.position -= 1;
+      return this.taskList.getTasks()[this.position];
+    }
+    index(): number {
+      return this.position;
+    }
+  }
+
+  const taskList = new TaskList();
+
+  taskList.addTask(new Task(8));
+  taskList.addTask(new Task(1));
+  taskList.addTask(new Task(3));
+
+  const iterator = taskList.getIterator();
+  /*
+  console.log(iterator.current());
+  console.log(iterator.next());
+  console.log(iterator.current());
+  console.log(iterator.prev());
+  console.log(iterator.index());
+  */
+})();
